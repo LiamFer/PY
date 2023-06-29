a6eaf066620da6d489d728777dde701afdd6efdc
William Fernandes <wiilfern1910@outlook.com>
2023-06-29 10:25:10 -0300

    Studying Variables

---
+++ Variables/variables_1.py
@@ -0,0 +1,32 @@
+# In this chapter you’ll learn about the different
+# kinds of data you can work with in
+# your Python programs. You’ll also learn how
+# to store your data in variables and how to use
+# those variables in your programs.
+
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+# Write a separate program to accomplish each of these exercises. Save
+# each program with a filename that follows standard Python conventions,
+# using lowercase letters and underscores, such as simple_message.py and
+# simple_messages.py.
+
+# 2-1. Simple Message: Store a message in a variable, and then print that
+# message.
+
+mensagem_1 = 'Estudando Python usando o livro "Python Crash Course"!'
+print(mensagem_1)
+
+# 2-2. Simple Messages: Store a message in a variable, and print that message.
+# Then change the value of your variable to a new message, and print the new
+# message.
+
+mensagem_2 = 'Simple Messages!'
+print(mensagem_2)
+
+mensagem_2 = "Understanding the variable changing it's content"
+print(mensagem_2)
