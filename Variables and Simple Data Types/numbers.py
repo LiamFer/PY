553758b07f8d86d1c4132bdf77bdab0721ed4164
William Fernandes <wiilfern1910@outlook.com>
2023-06-29 11:32:49 -0300

    Numbers

---
+++ Variables and Simple Data Types/numbers.py
@@ -0,0 +1,61 @@
+
+# Integers
+# You can add (+), subtract (-), multiply (*), and divide (/) integers in Python.
+
+print( 5 + 4 )
+print( 12 - 6 )
+print( 30 * 5 )
+print( 3 / 2)
+
+# Python uses two multiplication symbols to represent exponents:
+
+print ( 5 ** 3) # -> 5 * 5 * 5 = 125
+
+# Python supports the order of operations:
+
+print( ((7+3) * 4) / 2 ) 
+
+# Floats -> Python calls any number with a decimal point a float.
+
+print(0.2 + 0.1)
+
+# Avoiding Type Errors with the str() Function
+
+age = 19
+print ("Happy " + str(age) + "rd Birthday!")
+
+
+# 2-8. Number Eight: Write addition, subtraction, multiplication, and division
+# operations that each result in the number 8. Be sure to enclose your operations
+# in print statements to see the results. You should create four lines that look
+# like this: print(5 + 3)
+# Your output should simply be four lines with the number 8 appearing once
+# on each line.
+
+print ( 4 + 4 )
+print ( 17 - 9 )
+print ( (3 * 2) + 2 )
+print ( 32 / 4)
+print ( 2 ** 3)
+
+# 2-9. Favorite Number: Store your favorite number in a variable. Then, using
+# that variable, create a message that reveals your favorite number. Print that
+# message.
+
+favorite_number = 19
+print("My favorite number is " + str(favorite_number) + '!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
