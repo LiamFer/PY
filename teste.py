70c295bce2740eca53e4bb18d386eb9820a0e72d
William Fernandes <wiilfern1910@outlook.com>
2023-08-01 16:33:39 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,36 +1,25 @@
 
-items = 'box 1,'
-items+= 'box 2,'
+def reset(lista):
+    text = ''
+    for box in lista:
+        text += box + ','
+    return text
 
-# Pegar a string e transformar em um array tirando a última virgula
+items = 'box 1,box 2,'
+
+items += input('Insira uma nova Box: ') + ','
+
 array = items[:-1].split(',')
 print (array)
 
-# Voltar o array para uma String
+if 'box 3' in array:
+    array.remove('box 3')
+    items = reset(array)
+    print(array)
 
-items = ''
-for item in array:
-    items += item + ','
-items += 'box 3,'
-items += 'box 4,'
-items += 'box 5,'
-print ("Esse é o array string -> " + items)
-
-array = items[:-1].split(',')
-
-print (array)
-items = ''
-for item in array:
-    items += item + ','
 print (items)
 
-# if 'box 1' in array:
-#     array.remove('box 1')
-#     print(array)
-#     items = ''
-#     for box in array:
-#         items += 'box 1' + ','
-#     print (items)
-#     print(items[:-1].split(','))
-#     print()
+array = items[:-1].split(',')
+
+print (array)
 
