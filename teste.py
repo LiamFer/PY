17cc3f3cf2f4f8ef67bbb7d28438bc52025d7859
William Fernandes <wiilfern1910@outlook.com>
2023-08-01 15:15:09 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,32 @@
+
+items = 'box 1,'
+items+= 'box 2,'
+
+# Pegar a string e transformar em um array tirando a última virgula
+array = items[:-1].split(',')
+print (array)
+
+# Voltar o array para uma String
+
+items = ''
+for item in array:
+    items += item + ','
+items += 'box 3,'
+
+print ("Esse é o array string -> " + items)
+
+array = items[:-1].split(',')
+
+print (array)
+
+
+# if 'box 1' in array:
+#     array.remove('box 1')
+#     print(array)
+#     items = ''
+#     for box in array:
+#         items += 'box 1' + ','
+#     print (items)
+#     print(items[:-1].split(','))
+#     print()
+
